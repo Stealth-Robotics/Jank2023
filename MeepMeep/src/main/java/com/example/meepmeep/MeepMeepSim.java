2da3905a3df511948ea841c05dbea7322d0e234d
oliverstivers <77082348+oliverstivers@users.noreply.github.com>
2023-09-24 23:58:16 -0700

    updated meep meep with methods from old meep meep
    paths still need to be changed because they were made on old field background :( added in methods from now deleted meepmeep testing

--- MeepMeep/src/main/java/com/example/meepmeep/MeepMeepSim.java
+++ MeepMeep/src/main/java/com/example/meepmeep/MeepMeepSim.java
@@ -1,27 +1,50 @@
 package com.example.meepmeep;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.constraints.AngularVelocityConstraint;
+import com.acmerobotics.roadrunner.trajectory.constraints.MecanumVelocityConstraint;
+import com.acmerobotics.roadrunner.trajectory.constraints.MinVelocityConstraint;
+import com.acmerobotics.roadrunner.trajectory.constraints.ProfileAccelerationConstraint;
+import com.acmerobotics.roadrunner.trajectory.constraints.TrajectoryAccelerationConstraint;
+import com.acmerobotics.roadrunner.trajectory.constraints.TrajectoryVelocityConstraint;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
+import java.util.Arrays;
+
 public class MeepMeepSim{
+    private static TrajectoryVelocityConstraint veloConstraint(double angVel, double velo){
+        return new MinVelocityConstraint(Arrays.asList(
+                new AngularVelocityConstraint(angVel),
+                new MecanumVelocityConstraint(velo, 15.5)
+        ));
+    }
+    private static TrajectoryAccelerationConstraint accelConstraint(double constraint){
+        return new ProfileAccelerationConstraint(constraint);
+    }
     public static void main(String[] args) {
-        MeepMeep meepMeep = new MeepMeep(800);
+        MeepMeep meepMeep = new MeepMeep(600);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .setConstraints(80, 50, Math.toRadians(180), Math.toRadians(180), 15.5)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
+                        drive.trajectorySequenceBuilder(new Pose2d(62.17, -34.18, Math.toRadians(90.00)))
+                                .splineTo(new Vector2d(32.05, -24), Math.toRadians(185.67))
+                                .back(7, veloConstraint(Math.toRadians(180), 5), accelConstraint(5))
+                                .splineToLinearHeading(new Pose2d(34.3, -24, Math.toRadians(270)),
+                                        Math.toRadians(270),
+                                        veloConstraint(Math.toRadians(60), 50),
+                                        accelConstraint(40)
+                                )
+
+                                .lineTo(
+                                        new Vector2d(37.82, 52.99),
+                                        veloConstraint(2, 75),
+                                        accelConstraint(50)
+                                )
                                 .build()
                 );
 
