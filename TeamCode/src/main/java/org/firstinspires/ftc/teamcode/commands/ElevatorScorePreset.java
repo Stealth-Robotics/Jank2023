9203d66b74fff6268b8781354149b75e3a694583
oliverstivers <77082348+oliverstivers@users.noreply.github.com>
2023-10-05 10:59:12 -0700

    fixed lost files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/ElevatorScorePreset.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.commands;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+
+import org.firstinspires.ftc.teamcode.subsystems.ElevatorSubsystem;
+
+import java.util.function.DoubleSupplier;
+
+public class ElevatorScorePreset extends CommandBase {
+    private ElevatorSubsystem elevator;
+    private DoubleSupplier input;
+
+    //use this for teleop
+    public ElevatorScorePreset(ElevatorSubsystem elevator, DoubleSupplier input) {
+        this.elevator = elevator;
+        this.input = input;
+    }
+
+    //use this for autonomous
+    public ElevatorScorePreset(ElevatorSubsystem elevator) {
+        this.elevator = elevator;
+    }
+
+    @Override
+    public void initialize() {
+        elevator.setUsePID(true);
+        //todo: find setpoint
+        elevator.setSetpoint(0);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return (elevator.atSetpoint() || (input != null && Math.abs(input.getAsDouble()) > 0.05));
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ElevatorSubsystem.java
@@ -0,0 +1,60 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.arcrobotics.ftclib.controller.PIDFController;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class ElevatorSubsystem extends SubsystemBase {
+    private DcMotor motor1;
+    private DcMotor motor2;
+    //TODO: Tune PID
+    private final PIDFController elevatorPID = new PIDFController(0, 0, 0, 0);
+    private boolean usePID = true;
+
+    public ElevatorSubsystem(HardwareMap hardwareMap) {
+        motor1 = hardwareMap.get(DcMotor.class, "elevatorMotor1");
+        motor2 = hardwareMap.get(DcMotor.class, "elevatorMotor2");
+
+        motor1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motor2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        elevatorPID.setTolerance(10);
+
+    }
+
+    public void setSetpoint(double pos) {
+        elevatorPID.setSetPoint(pos);
+    }
+
+    public boolean atSetpoint() {
+        return elevatorPID.atSetPoint();
+    }
+
+    public double getEncoderPosition() {
+        return motor1.getCurrentPosition();
+    }
+
+    public void setToCurrentPosition() {
+        elevatorPID.setSetPoint(getEncoderPosition());
+    }
+
+    public void setPower(double power) {
+        motor1.setPower(power);
+        motor2.setPower(power);
+    }
+
+    public void setUsePID(boolean usePID) {
+        this.usePID = usePID;
+    }
+
+    //TODO: Tune PID
+    //call setUsePID(true) to use PID
+    @Override
+    public void periodic() {
+        if (usePID) {
+            double power = elevatorPID.calculate(getEncoderPosition());
+            setPower(power);
+        }
+    }
+}
