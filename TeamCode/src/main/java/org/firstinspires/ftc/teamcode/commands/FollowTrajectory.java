c61a10fa3333c51c11004b2f0c65a937b6ec1dc3
oliverstivers <77082348+oliverstivers@users.noreply.github.com>
2023-10-20 23:51:46 -0700

    lots of auto things

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/FollowTrajectory.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode.commands;
+
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.arcrobotics.ftclib.command.CommandBase;
+
+import org.firstinspires.ftc.teamcode.roadrunner.trajectorysequence.TrajectorySequence;
+import org.firstinspires.ftc.teamcode.subsystems.DriveSubsystem;
+
+public class FollowTrajectory extends CommandBase {
+    private DriveSubsystem driveSubsystem;
+    private Trajectory trajectory;
+
+    public FollowTrajectory(DriveSubsystem driveSubsystem, Trajectory trajectory){
+        this.driveSubsystem = driveSubsystem;
+        this.trajectory = trajectory;
+        addRequirements(driveSubsystem);
+    }
+
+    @Override
+    public void initialize(){
+        driveSubsystem.followTrajectoryAsync(trajectory);
+    }
+    @Override
+    public void execute(){
+        driveSubsystem.update();
+    }
+    @Override
+    public boolean isFinished(){
+        return driveSubsystem.isBusy();
+    }
+
+    @Override
+    public void end(boolean interrupted){
+        driveSubsystem.stop();
+    }
+
+}
